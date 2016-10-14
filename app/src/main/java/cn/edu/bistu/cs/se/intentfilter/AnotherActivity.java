318e84d59873977baab6a80f3990fb75e3521fb1
TangRui666 <654427660@qq.com>
2016-10-14 17:10:55 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/intentfilter/AnotherActivity.java
@@ -0,0 +1,20 @@
+package cn.edu.bistu.cs.se.intentfilter;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class AnotherActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.another_layout);
+
+    }
+}
